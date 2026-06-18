da311caea00008d2d3c06725701e0fb614d79aee
hedizair <hedi.marwan@hotmail.fr>
2026-06-18 21:08:47 +0200

    init mvn

---
+++ src/main/java/com/hedizair/soundboard/App.java
@@ -0,0 +1,13 @@
+package com.hedizair.soundboard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/main/java/com/hedizair/soundbox/Main.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.hedizair.soundbox;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, SoundBox!");
-    }
-}
